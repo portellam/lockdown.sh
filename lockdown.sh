bcc40b6c2c233e4f3b76ba6e98c3cfb2241a1582
portellam <amportell.13@gmail.com>
2024-07-05 03:28:12 -0400

    Update lockdown.sh

--- lockdown.sh
+++ lockdown.sh
@@ -18,9 +18,227 @@
 # - [ ] determine package manager and run updates or installs.
 
 #
+# DESC:   Prompt the user to execute the command.
+# $1:     the command name as a string.
+# $2:     the prompt as a string.
+# RETURN: If the prompt is answered and the command passes, return 0.
+#         If the command fails, return 1.
+#         If the prompt is not answered, return 255.
+#
+  function run
+  {
+    local -r str_command="${1}"
+    local -r str_prompt="${2}"
+
+    typeset -f "${str_command}" | tail --lines +2
+
+    echo -e "${str_prompt}"
+    echo -en "$0: Run the above command(s)? [Y/n]: "
+
+    read -r str_answer
+
+    if [ "${str_answer}" != "${answer#[Yy]}" ] ;then
+      echo -e "$0: Skipped command(s)."
+      return 255
+    fi
+
+    if ! ${str_command}; then
+      echo -e "$0: Failure."
+      return 1
+    fi
+
+    echo -e "$0: Success."
+    return 0
+  }
+
+#
+# DESC:   Main execution.
+# RETURN: If all prompted commands pass, return 0.
+#         If one or more command(s) fail, return 1.
+#
+  function main
+  {
+    local -Ar dict_command_prompts=(
+      ["apt_update"]="Update and upgrade all packages"
+      ["configure_iptables"]="Configure iptables"
+      ["install_fail2ban"]="Install fail2ban"
+      ["configure_kernel"]="Configure kernel"
+      ["automatic_updates"]="Setup automatic updates"
+      ["configure_auditd"]="Setup auditd"
+      ["disable_core_dumps"]="Disable core dumps"
+      ["restrict_login"]="Restrict login"
+      ["secure_ssh"]="Secure ssh"
+      ["create_admin_user"]="Create admin user"
+      ["add_legal_banner"]="Add legal banner"
+      ["install_recommended_packages"]="Install recommended packages"
+      ["setup_aide"]="Setup aide"
+      ["enable_process_accounting"]="Enable process accounting"
+      ["disable_uncommon_filesystems"]="Disable unused filesystems"
+      ["disable_firewire"]="Disable firewire"
+      ["disable_usb"]="Disable usb"
+      ["disable_uncommon_protocols"]="Disable uncommon protocols"
+      ["change_root_permissions"]="Change root dir permissions"
+      ["restrict_access_to_compilers"]="Restrict access to compilers"
+      ["move_tmp_to_tmpfs"]="Move tmp to tmpfs"
+
+      ["remount_dir_with_restrictions"]="Remount /tmp /proc /dev /run to be more "\
+        "restrictive"
+
+      ["purge_old_packages"]="Purge old packages"
+      ["reboot"]="Reboot"
+    )
+
+    for str_key in "${!dict_command_prompts[@]}"; do
+      local str_value="${dict_command_prompts["${str_key}"]}"
+
+      run "${str_key}" "${str_value}"
+
+      if [[ "${?}" -eq 1 ]]; then
+        echo -e "$0: Script has failed."
+        return 1
+      fi
+    done
+
+    echo -e "$0: Script finished successfully."
+    return 0
+  }
+
+#
 # DESC: Additions
 #
+  #
+  # RETURN: If successful, return 0.
+  #         If not successful, return 1.
+  #
+    function apt_update
+    {
+      # Update package list
+      apt update || return 1
 
+      # Apt upgrade packages
+      apt upgrade -y || return 1
+
+      # Apt full upgrade
+      apt full-upgrade -y || return 1
+    }
+
+  #
+  # RETURN: Return code from last statement.
+  #
+    function configure_iptables
+    {
+      # iptables
+      apt install -y iptables-persistent
+
+      # Flush existing rules
+      iptables -F
+
+      # Defaults
+      iptables -P INPUT DROP
+      iptables -P FORWARD DROP
+      iptables -P OUTPUT ACCEPT
+
+      # Accept loopback input
+      iptables -A INPUT -i lo -p all -j ACCEPT
+
+      # Allow three-way Handshake
+      iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+      # Stop Masked Attacks
+      iptables -A INPUT -p icmp --icmp-type 13 -j DROP
+      iptables -A INPUT -p icmp --icmp-type 17 -j DROP
+      iptables -A INPUT -p icmp --icmp-type 14 -j DROP
+      iptables -A INPUT -p icmp -m limit --limit 1/second -j ACCEPT
+
+      # Discard invalid Packets
+      iptables -A INPUT -m state --state INVALID -j DROP
+      iptables -A FORWARD -m state --state INVALID -j DROP
+      iptables -A OUTPUT -m state --state INVALID -j DROP
+
+      # Drop Spoofing attacks
+      iptables -A INPUT -d 0.0.0.0/8 -j DROP
+      iptables -A INPUT -d 239.255.255.0/24 -j DROP
+      iptables -A INPUT -d 224.0.0.0/4 -j DROP
+      iptables -A INPUT -d 240.0.0.0/5 -j DROP
+      iptables -A INPUT -d 255.255.255.255 -j DROP
+      iptables -A INPUT -s 0.0.0.0/8 -j DROP
+      iptables -A INPUT -s 10.0.0.0/8 -j DROP
+      iptables -A INPUT -s 127.0.0.0/8 -j DROP
+      iptables -A INPUT -s 169.254.0.0/16 -j DROP
+      iptables -A INPUT -s 172.16.0.0/12 -j DROP
+      iptables -A INPUT -s 192.168.0.0/24 -j DROP
+      iptables -A INPUT -s 224.0.0.0/4 -j DROP
+      iptables -A INPUT -s 240.0.0.0/5 -j DROP
+
+      # Drop packets with excessive RST to avoid Masked attacks
+      iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second \
+        --limit-burst 2 -j ACCEPT
+
+      # Block ips doing portscan for 24 hours
+      iptables -A INPUT   -m recent --name portscan --rcheck --seconds 86400 -j DROP
+      iptables -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP
+
+      # After 24 hours remove IP from block list
+      iptables -A INPUT   -m recent --name portscan --remove
+      iptables -A FORWARD -m recent --name portscan --remove
+
+      # Allow ssh
+      iptables -A INPUT -p tcp -m tcp --dport 141 -j ACCEPT
+
+      # Allow Ping
+      iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
+
+      # Allow one ssh connection at a time
+      iptables -A INPUT -p tcp --syn --dport 141 -m connlimit --connlimit-above 2 -j \
+        REJECT
+
+      iptables-save > /etc/iptables/rules.v4
+      ip6tables-save > /etc/iptables/rules.v6
+    }
+
+  #
+  # RETURN: If successful, return 0.
+  #         If not successful, return 1.
+  #
+    function configure_kernel
+    {
+      echo -e \
+        "net.ipv4.conf.all.accept_redirects: 0\n"\
+        "net.ipv4.conf.all.accept_source_route: 0\n"\
+        "net.ipv4.conf.all.log_martians: 1\n"\
+        "net.ipv4.conf.all.rp_filter: 1\n"\
+        "net.ipv4.conf.all.secure_redirects: 1\n"\
+        "net.ipv4.conf.all.send_redirects: 0\n"\
+        "net.ipv4.conf.default.accept_redirects: 0\n"\
+        "net.ipv4.conf.default.accept_source_route: 0\n"\
+        "net.ipv4.conf.default.log_martians: 1\n"\
+        "net.ipv4.conf.default.rp_filter: 1\n"\
+        "net.ipv4.conf.default.secure_redirects: 1\n"\
+        "net.ipv4.conf.default.send_redirects: 0\n"\
+        "net.ipv4.icmp_echo -e_ignore_broadcasts: 1\n"\
+        "net.ipv4.icmp_ignore_bogus_error_responses: 1\n"\
+        "net.ipv4.icmp_echo -e_ignore_all: 0\n"\
+        "net.ipv4.ip_forward: 0\n"\
+        "net.ipv4.tcp_rfc1337: 1\n"\
+        "net.ipv4.tcp_syncookies: 1\n"\
+        "net.ipv6.conf.all.accept_redirects: 0\n"\
+        "net.ipv6.conf.all.forwarding: 0\n"\
+        "net.ipv6.conf.all.accept_source_route: 0\n"\
+        "net.ipv6.conf.default.accept_redirects: 0\n"\
+        "net.ipv6.conf.default.accept_source_route: 0\n"\
+        "fs.protected_hardlinks: 1\n"\
+        "fs.protected_symlinks: 1\n"\
+        "kernel.core_uses_pid: 1\n"\
+        "kernel.perf_event_paranoid: 2\n"\
+        "kernel.kptr_restrict: 2\n"\
+        "kernel.randomize_va_space: 2\n"\
+        "kernel.sysrq: 0\n"\
+        "kernel.yama.ptrace_scope: 1" \
+
+        > /etc/sysctl.d/80-lockdown.conf || return 1
+
+      sysctl --system || return 1
+    }
 
 #
 # DESC: Access Restrictions
@@ -30,154 +248,93 @@
 #
 # DESC: Installs
 #
-
+  #
+  # RETURN: Return code from last statement.
+  #
+    function install_fail2ban
+    {
+      apt install -y fail2ban
+    }
 
 #
 # DESC: Removals
 #
+  #
+  # RETURN: If successful, return 0.
+  #         If not successful, return 1.
+  #
+    function disable_core_dumps
+    {
+      # Disable core dumps
+      echo -e "* hard core 0" >> /etc/security/limits.conf || return 1
 
+      echo -e \
+        "ProcessSizeMax=0\n"
+        "Storage=none" \
+        >> /etc/systemd/coredump.conf || return 1
 
-#
-# RETURN: If successful, return 0.
-#         If not successful, return 1.
-#
-  function apt_update
-  {
-    # Update package list
-    apt update || return 1
+      echo -e "ulimit -c 0" >> /etc/profile || return 1
+    }
 
-    # Apt upgrade packages
-    apt upgrade -y || return 1
+  #
+  # RETURN: Return code from last statement.
+  #
+    function disable_uncommon_filesystems
+    {
+      # Disable uncommon filesystems
+      echo -e "install cramfs /bin/true\n"\
+        "install freevxfs /bin/true\n"\
+        "install hfs /bin/true\n"\
+        "install hfsplus /bin/true\n"\
+        "install jffs2 /bin/true\n"\
+        "install squashfs /bin/true" \
+        >> /etc/modprobe.d/filesystems.conf
+    }
 
-    # Apt full upgrade
-    apt full-upgrade -y || return 1
-  }
+  #
+  # RETURN: Return code from last statement.
+  #
+    function disable_firewire
+    {
+      echo -e \
+        "install udf /bin/true\n"\
+        "blacklist firewire-core\n"\
+        "blacklist firewire-ohci\n"\
+        "blacklist firewire-sbp2" \
+        >> /etc/modprobe.d/blacklist.conf
+    }
 
-#
-# RETURN: Return code from last statement.
-#
-  function configure_iptables
-  {
-    # iptables
-    apt install -y iptables-persistent
+  #
+  # RETURN: Return code from last statement.
+  #
+    function disable_uncommon_protocols
+    {
+      echo -e \
+        "install sctp /bin/true\n"\
+        "install dccp /bin/true\n"\
+        "install rds /bin/true\n"\
+        "install tipc /bin/true" \
+        >> /etc/modprobe.d/protocols.conf
+    }
 
-    # Flush existing rules
-    iptables -F
+  #
+  # RETURN: Return code from last statement.
+  #
+    function disable_usb
+    {
+      echo -e "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf
+    }
 
-    # Defaults
-    iptables -P INPUT DROP
-    iptables -P FORWARD DROP
-    iptables -P OUTPUT ACCEPT
-
-    # Accept loopback input
-    iptables -A INPUT -i lo -p all -j ACCEPT
-
-    # Allow three-way Handshake
-    iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-    # Stop Masked Attacks
-    iptables -A INPUT -p icmp --icmp-type 13 -j DROP
-    iptables -A INPUT -p icmp --icmp-type 17 -j DROP
-    iptables -A INPUT -p icmp --icmp-type 14 -j DROP
-    iptables -A INPUT -p icmp -m limit --limit 1/second -j ACCEPT
-
-    # Discard invalid Packets
-    iptables -A INPUT -m state --state INVALID -j DROP
-    iptables -A FORWARD -m state --state INVALID -j DROP
-    iptables -A OUTPUT -m state --state INVALID -j DROP
-
-    # Drop Spoofing attacks
-    iptables -A INPUT -d 0.0.0.0/8 -j DROP
-    iptables -A INPUT -d 239.255.255.0/24 -j DROP
-    iptables -A INPUT -d 224.0.0.0/4 -j DROP
-    iptables -A INPUT -d 240.0.0.0/5 -j DROP
-    iptables -A INPUT -d 255.255.255.255 -j DROP
-    iptables -A INPUT -s 0.0.0.0/8 -j DROP
-    iptables -A INPUT -s 10.0.0.0/8 -j DROP
-    iptables -A INPUT -s 127.0.0.0/8 -j DROP
-    iptables -A INPUT -s 169.254.0.0/16 -j DROP
-    iptables -A INPUT -s 172.16.0.0/12 -j DROP
-    iptables -A INPUT -s 192.168.0.0/24 -j DROP
-    iptables -A INPUT -s 224.0.0.0/4 -j DROP
-    iptables -A INPUT -s 240.0.0.0/5 -j DROP
-
-    # Drop packets with excessive RST to avoid Masked attacks
-    iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second \
-      --limit-burst 2 -j ACCEPT
-
-    # Block ips doing portscan for 24 hours
-    iptables -A INPUT   -m recent --name portscan --rcheck --seconds 86400 -j DROP
-    iptables -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP
-
-    # After 24 hours remove IP from block list
-    iptables -A INPUT   -m recent --name portscan --remove
-    iptables -A FORWARD -m recent --name portscan --remove
-
-    # Allow ssh
-    iptables -A INPUT -p tcp -m tcp --dport 141 -j ACCEPT
-
-    # Allow Ping
-    iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
-
-    # Allow one ssh connection at a time
-    iptables -A INPUT -p tcp --syn --dport 141 -m connlimit --connlimit-above 2 -j \
-      REJECT
-
-    iptables-save > /etc/iptables/rules.v4
-    ip6tables-save > /etc/iptables/rules.v6
-  }
-
-#
-# RETURN: Return code from last statement.
-#
-  function install_fail2ban
-  {
-    apt install -y fail2ban
-  }
-
-#
-# RETURN: If successful, return 0.
-#         If not successful, return 1.
-#
-  function configure_kernel
-  {
-    echo -e \
-      "net.ipv4.conf.all.accept_redirects: 0\n"\
-      "net.ipv4.conf.all.accept_source_route: 0\n"\
-      "net.ipv4.conf.all.log_martians: 1\n"\
-      "net.ipv4.conf.all.rp_filter: 1\n"\
-      "net.ipv4.conf.all.secure_redirects: 1\n"\
-      "net.ipv4.conf.all.send_redirects: 0\n"\
-      "net.ipv4.conf.default.accept_redirects: 0\n"\
-      "net.ipv4.conf.default.accept_source_route: 0\n"\
-      "net.ipv4.conf.default.log_martians: 1\n"\
-      "net.ipv4.conf.default.rp_filter: 1\n"\
-      "net.ipv4.conf.default.secure_redirects: 1\n"\
-      "net.ipv4.conf.default.send_redirects: 0\n"\
-      "net.ipv4.icmp_echo -e_ignore_broadcasts: 1\n"\
-      "net.ipv4.icmp_ignore_bogus_error_responses: 1\n"\
-      "net.ipv4.icmp_echo -e_ignore_all: 0\n"\
-      "net.ipv4.ip_forward: 0\n"\
-      "net.ipv4.tcp_rfc1337: 1\n"\
-      "net.ipv4.tcp_syncookies: 1\n"\
-      "net.ipv6.conf.all.accept_redirects: 0\n"\
-      "net.ipv6.conf.all.forwarding: 0\n"\
-      "net.ipv6.conf.all.accept_source_route: 0\n"\
-      "net.ipv6.conf.default.accept_redirects: 0\n"\
-      "net.ipv6.conf.default.accept_source_route: 0\n"\
-      "fs.protected_hardlinks: 1\n"\
-      "fs.protected_symlinks: 1\n"\
-      "kernel.core_uses_pid: 1\n"\
-      "kernel.perf_event_paranoid: 2\n"\
-      "kernel.kptr_restrict: 2\n"\
-      "kernel.randomize_va_space: 2\n"\
-      "kernel.sysrq: 0\n"\
-      "kernel.yama.ptrace_scope: 1" \
-
-      > /etc/sysctl.d/80-lockdown.conf || return 1
-
-    sysctl --system || return 1
-  }
+  #
+  # RETURN: If successful, return 0.
+  #         If not successful, return 1.
+  #
+    function purge_old_packages
+    {
+      # Purge old/removed packages
+      apt autoremove -y || return 1
+      apt purge -y "$( dpkg --list | grep '^rc' | awk '{print $2}' )" || return 1
+    }
 
 function automatic_updates
 {
@@ -330,15 +487,6 @@ function configure_auditd
   service auditd restart
 }
 
-function disable_core_dumps
-{
-  # Disable core dumps
-  echo -e "* hard core 0" >> /etc/security/limits.conf
-  echo -e "ProcessSizeMax=0
-  Storage=none" >> /etc/systemd/coredump.conf
-  echo -e "ulimit -c 0" >> /etc/profile
-}
-
 function restrict_login
 {
   # Set login.defs
@@ -433,43 +581,6 @@ function enable_process_accounting
   systemctl start acct.service
 }
 
-function disable_uncommon_filesystems
-{
-  # Disable uncommon filesystems
-  echo -e "install cramfs /bin/true\n"\
-    "install freevxfs /bin/true\n"\
-    "install hfs /bin/true\n"\
-    "install hfsplus /bin/true\n"\
-    "install jffs2 /bin/true\n"\
-    "install squashfs /bin/true" \
-  >> /etc/modprobe.d/filesystems.conf
-}
-
-function disable_firewire
-{
-  echo -e \
-    "install udf /bin/true\n"\
-    "blacklist firewire-core\n"\
-    "blacklist firewire-ohci\n"\
-    "blacklist firewire-sbp2" \
-    >> /etc/modprobe.d/blacklist.conf
-}
-
-function disable_usb
-{
-  echo -e "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf
-}
-
-function disable_uncommon_protocols
-{
-  echo -e \
-    "install sctp /bin/true\n"\
-    "install dccp /bin/true\n"\
-    "install rds /bin/true\n"\
-    "install tipc /bin/true" \
-    >> /etc/modprobe.d/protocols.conf
-}
-
 function change_root_permissions
 {
  # Change /root permissions
@@ -503,99 +614,6 @@ function remount_dir_with_restrictions
   # Mount /run as nodev
   mount -o remount,nodev /run
 }
-
-function purge_old_packages
-{
-  # Purge old/removed packages
-  apt autoremove -y || return 1
-  apt purge -y "$( dpkg --list | grep '^rc' | awk '{print $2}' )"
-}
-
-#
-# DESC:   Prompt the user to execute the command.
-# $1:     the command name as a string.
-# $2:     the prompt as a string.
-# RETURN: If the prompt is answered and the command passes, return 0.
-#         If the command fails, return 1.
-#         If the prompt is not answered, return 255.
-#
-  function run
-  {
-    local -r str_command="${1}"
-    local -r str_prompt="${2}"
-
-    typeset -f "${str_command}" | tail --lines +2
-
-    echo -e "${str_prompt}"
-    echo -en "$0: Run the above command(s)? [Y/n]: "
-
-    read -r str_answer
-
-    if [ "${str_answer}" != "${answer#[Yy]}" ] ;then
-      echo -e "$0: Skipped command(s)."
-      return 255
-    fi
-
-    if ! ${str_command}; then
-      echo -e "$0: Failure."
-      return 1
-    fi
-
-    echo -e "$0: Success."
-    return 0
-  }
-
-#
-# DESC:   Main execution.
-# RETURN: If all prompted commands pass, return 0.
-#         If one or more command(s) fail, return 1.
-#
-  function main
-  {
-    local -Ar dict_command_prompts=(
-      ["apt_update"]="Update and upgrade all packages"
-      ["configure_iptables"]="Configure iptables"
-      ["install_fail2ban"]="Install fail2ban"
-      ["configure_kernel"]="Configure kernel"
-      ["automatic_updates"]="Setup automatic updates"
-      ["configure_auditd"]="Setup auditd"
-      ["disable_core_dumps"]="Disable core dumps"
-      ["restrict_login"]="Restrict login"
-      ["secure_ssh"]="Secure ssh"
-      ["create_admin_user"]="Create admin user"
-      ["add_legal_banner"]="Add legal banner"
-      ["install_recommended_packages"]="Install recommended packages"
-      ["setup_aide"]="Setup aide"
-      ["enable_process_accounting"]="Enable process accounting"
-      ["disable_uncommon_filesystems"]="Disable unused filesystems"
-      ["disable_firewire"]="Disable firewire"
-      ["disable_usb"]="Disable usb"
-      ["disable_uncommon_protocols"]="Disable uncommon protocols"
-      ["change_root_permissions"]="Change root dir permissions"
-      ["restrict_access_to_compilers"]="Restrict access to compilers"
-      ["move_tmp_to_tmpfs"]="Move tmp to tmpfs"
-
-      ["remount_dir_with_restrictions"]="Remount /tmp /proc /dev /run to be more "\
-        "restrictive"
-
-      ["purge_old_packages"]="Purge old packages"
-      ["reboot"]="Reboot"
-    )
-
-    for str_key in "${!dict_command_prompts[@]}"; do
-      local str_value="${dict_command_prompts["${str_key}"]}"
-
-      run "${str_key}" "${str_value}"
-
-      if [[ "${?}" -eq 1 ]]; then
-        echo -e "$0: Script has failed."
-        return 1
-      fi
-    done
-
-    echo -e "$0: Script finished successfully."
-    return 0
-  }
 
 #
 # Main
